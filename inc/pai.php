76c6a3a8c8af6cb17d28348a74a869b3ef754662
Freddy Lundekvam <freddy@wearelogik.com>
2014-10-07 08:13:37 +0800

    Deprecated notice on index

--- inc/pai.php
+++ inc/pai.php
@@ -5,16 +5,16 @@ function pai_get_include($__pai_file) {
 	ob_start();
 	include($__pai_file);
 	$content = ob_get_clean();
-	
+
 	return $content;
 }
 
 
 function pai_js_files() {
 	$conf = pai_conf('ajax');
-	
+
 	$files = array();
-	
+
 	$files[] = PAI_FOLDER.'/deps/HashHistory/HashHistory.js';
 	$files[] = PAI_FOLDER.'/js-src/adapter.'.$conf['framework'].'.js';
 	$files[] = PAI_FOLDER.'/js-src/pai.js';
@@ -25,30 +25,30 @@ function pai_js_files() {
 		if (@$plugin['js-adapters']) {
 			if ($plugin['js-adapters'][ $conf['framework'] ]) {
 				$files[] = $path.$plugin['js-adapters'][ $conf['framework'] ];
-			} 
+			}
 			else {
 				$files[] = $path.reset($plugin['js-adapters']);
 			}
 		}
-		foreach((array) @$plugin['js'] AS $file) { 
+		foreach((array) @$plugin['js'] AS $file) {
 			$files[] = $path.$file;
 		}
 	}
-	
+
 	$files = pai_apply_filters('jsfiles', $files);
-	
+
 	return $files;
 }
 
 function pai_head() {
-	if(pai_conf('expose_pai')) { 
+	if(pai_conf('expose_pai')) {
 		?><meta name="generator" content="PAI/<?php echo PAI_VERSION; ?>">
-<?php 
+<?php
 	}
-	
+
 	?><script>(function(g){function p(){q.push(arguments)}var q=[];p.ready=function(){p._i(q)};g.PAI=p})(this);</script>
 <?php
-	
+
 	pai_do_action('head');
 }
 
@@ -56,7 +56,7 @@ function pai_foot() {
 
 //	echo '<script>' . file_get_contents(PAI_FILEPATH.'js-src'.DIRECTORY_SEPARATOR.'inline.js') . '</script>';
 	?>
-	
+
 <script>PAI.PAGE=<?php echo pai_json_encode(PAI_PAGE); ?>; PAI.LINK=<?php echo pai_json_encode(pai_link()); ?>;
 <?php
 	global $pai_js_options;
@@ -74,8 +74,8 @@ function pai_foot() {
 else: ?>
 <script src="<?php echo PAI_PATH.PAI_FOLDER; ?>/js.php?get=vars&content_folder=<?php echo PAI_FOLDER_CONTENT?>" defer></script>
 <script src="<?php echo PAI_PATH.PAI_FOLDER; ?>/js.php?get=files" defer></script>
-<?php endif; 
-	
+<?php endif;
+
 //	pai_do_action('scripts'); // backward
 	pai_do_action('footer');
 }
@@ -88,23 +88,23 @@ function pai_conf() {
 		if (is_file(PAI_FILEPATH_CONTENT.'pai.json')) {
 			$conf = pai_deep_merge($conf, pai_json_decode_file(PAI_FILEPATH_CONTENT.'pai.json', true));
 		}
-		
+
 		if (!$conf) {
 			pai_trigger_error('Failed to parse configuration file. Check your syntax.', E_USER_ERROR, 0);
 		}
-		
+
 
 		if (!is_string($conf['url']['path'])) {
 			$dr = realpath($_SERVER['DOCUMENT_ROOT']);
 			if (substr($dr, -1) == DIRECTORY_SEPARATOR) { $dr = substr($dr, 0, -1); }
 			$conf['url']['path'] = substr(dirname(PAI_FILEPATH), strlen($dr)).'/';
 		}
-		
-		
+
+
 		$conf = pai_apply_filters('config', $conf);
-		
+
 	}
-	
+
 	$ret = $conf;
 	$args = func_get_args();
 	foreach($args AS $arg) {
@@ -121,8 +121,8 @@ function pai_link($page = null, $parameters = array(), $noPath = false) {
 	$get = $_GET;
 	array_walk($get, create_function('&$value, $key', 'if (substr($key, 0, 4) == "pai_") { $value = null; }'));
 	$get = array_filter($get);
-	$q = http_build_query(array_merge($get, (array) $parameters)); 
-	
+	$q = http_build_query(array_merge($get, (array) $parameters));
+
 	if ($page === null) { $page = PAI_PAGE; }
 	if ($page == pai_conf('url', 'rootElement')) { $page = ''; }
 
@@ -136,9 +136,9 @@ function pai_link($page = null, $parameters = array(), $noPath = false) {
 
 function pai_title($return = false) {
 	$title = pai_pageInfo('title');
-	
+
 	$title = pai_apply_filters('title', $title);
-	
+
 	if ($return) {
 		return $title;
 	}
@@ -147,16 +147,16 @@ function pai_title($return = false) {
 
 function pai_pageInfo($key = null, $page = PAI_PAGE) {
 	static $info;
-	
+
 	// Load pages file
 	if (!$info) {
 		$info = pai_json_decode_file(PAI_FILEPATH_CONTENT.'pages.json');
-		
+
 		foreach($info AS $p => $pageinfo) {
 			foreach($pageinfo AS $k => $value) {
 				$skey = explode('-', $k);
 				if (count($skey) > 1) {
-					
+
 					$v = $info->$p;
 					$ref =& $v;
 					foreach($skey AS $a) {
@@ -168,10 +168,10 @@ function pai_pageInfo($key = null, $page = PAI_PAGE) {
 				}
 			}
 		}
-		
+
 		$info = pai_apply_filters('pageinfo', $info);
 	}
-	
+
 	// gjør $key til array for dyp søk
 	if ($key) {
 		if (!is_array($key)) {
@@ -181,12 +181,12 @@ function pai_pageInfo($key = null, $page = PAI_PAGE) {
 
 	// Hvis man vil ha alle verdier for alle sider
 	if (!$page) {
-		
+
 		// Hvis ikke $key, returner hele pages filen
 		if (!$key) {
 			return $info;
 		}
-		
+
 		// henter spesifik verdi fra alle sider
 		$result = (object) array();
 		foreach($info AS $page => $settings) {
@@ -194,7 +194,7 @@ function pai_pageInfo($key = null, $page = PAI_PAGE) {
 			foreach($key AS $k) {
 				if (isset($ret->$k)) {
 					$ret = $ret->$k;
-				} 
+				}
 				else {
 					unset($ret);
 					break;
@@ -206,12 +206,12 @@ function pai_pageInfo($key = null, $page = PAI_PAGE) {
 		}
 		return $result;
 	}
-	
+
 	if (!isset($info->$page)) {
 		pai_trigger_error("Undefined page <i>$page</i>", E_USER_NOTICE);
 		return null;
 	}
-	
+
 	if ($key) {
 		$ret = $info->$page;
 		foreach($key AS $k) {
@@ -229,7 +229,7 @@ function pai_pageInfo($key = null, $page = PAI_PAGE) {
 function pai_page($path) {
 
 	$path = strtolower(preg_replace(pai_conf('url', 'pathStripRegexp'), "", $path));
-	while ($path[0] === '/') $path = substr($path, 1);
+	while (@$path[0] === '/') $path = substr($path, 1);
 	if (!$path) { $path = pai_conf('url', 'rootElement'); }
 	if (substr($path, -1) == '/') { $path = substr($path, 0, -1); }
 
@@ -238,8 +238,8 @@ function pai_page($path) {
 
 	define('PAI_PAGE', $path);
 	define('PAI_SELF', pai_link());
-	
-	
+
+
 	return $path;
 }
 
@@ -249,7 +249,7 @@ function pai_set_js_options($key, $value = null) {
 	if (!is_array($key)) {
 		$key = explode('-', $key);
 	}
-	
+
 	$item = &$pai_js_options;
 	$array = false;
 	foreach($key AS $k) {
@@ -280,17 +280,17 @@ function pai_trigger_error($message, $level/* = E_USER_NOTICE*/, $tracelength = 
 		else {
 			$message = 'PAI: '.strip_tags($message).' in '.$callee['file'].' on line '.$callee['line'];
 		}
-		
+
 	}
-	
+
 	trigger_error($message, $level);
 }
 
 function pai_redirect($url, $time = 0, $external = false, $code = 302) {
-	
+
 	$time = (int) $time;
 	$code = (int) $code;
-	
+
 	if (PAI_AJAX) {
 		global $pai_ajax_response;
 		$pai_ajax_response['redirect'] = array('url' => $url, 'time' => $time, 'external' => (bool) $external);
